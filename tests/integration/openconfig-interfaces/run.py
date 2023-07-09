f34fa90951082b73c775bf4d4ab3db656cc8ec8e
Frédéric Perrin <fred@fperrin.net>
2023-07-09 21:05:11 +0100

    Fix integration/openconfig-interfaces tests
    Ping the version of openconfig/public to 8527e6426cf1, which is the
    most recent change to the relevant YANG model before the last update
    to expected output in the pyangbind tests.
    
    Also, patch two YANG models that trigger a pyang error (the same
    change has since been done in openconfig/public).

--- tests/integration/openconfig-interfaces/run.py
+++ tests/integration/openconfig-interfaces/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import os.path
 import unittest
 
@@ -28,7 +29,7 @@ class OpenconfigInterfacesTests(PyangBindTestCase):
         },
         {
             "local_path": "include",
-            "remote_prefix": "https://raw.githubusercontent.com/openconfig/public/master/release/models/",
+            "remote_prefix": "https://raw.githubusercontent.com/openconfig/public/8527e6426cf136930ff24f60939994949cefc073/release/models/",
             "files": [
                 "openconfig-extensions.yang",
                 "types/openconfig-types.yang",
@@ -40,7 +41,7 @@ class OpenconfigInterfacesTests(PyangBindTestCase):
         },
         {
             "local_path": "openconfig",
-            "remote_prefix": "https://raw.githubusercontent.com/openconfig/public/master/release/models/",
+            "remote_prefix": "https://raw.githubusercontent.com/openconfig/public/8527e6426cf136930ff24f60939994949cefc073/release/models/",
             "files": [
                 "interfaces/openconfig-if-ip.yang",
                 "interfaces/openconfig-if-ethernet.yang",
@@ -50,6 +51,29 @@ class OpenconfigInterfacesTests(PyangBindTestCase):
             ],
         },
     ]
+
+    @classmethod
+    def _fetch_remote_yang_files(cls):
+        super()._fetch_remote_yang_files()
+
+        # Fix the following pyang error:
+        # pyangbind/tests/integration/openconfig-interfaces/openconfig/openconfig-if-ethernet.yang:427:
+        # warning: node "openconfig-interfaces::state" is config false and is
+        # not part of the accessible tree
+
+        # This has since been fixed in openconfig/public:
+        # https://github.com/openconfig/public/commit/d31dd1a137022700daa9c3ec33e471c48fd431cf
+        # However, upgrading to the latest version of the openconfig models
+        # would require many changes to the expected output
+        need_fixes = ["openconfig-if-aggregate.yang", "openconfig-if-ethernet.yang"]
+        for dirpath, dirnames, filenames in os.walk(cls._test_path):
+            for need_fix in need_fixes:
+                if need_fix in filenames:
+                    with open(os.path.join(dirpath, need_fix), "r") as f:
+                        yang_text = f.read()
+                    yang_text = yang_text.replace("oc-if:state/oc-if:type", "oc-if:config/oc-if:type")
+                    with open(os.path.join(dirpath, need_fix), "w") as f:
+                        f.write(yang_text)
 
     def setUp(self):
         self.yang_helper = YANGPathHelper()
